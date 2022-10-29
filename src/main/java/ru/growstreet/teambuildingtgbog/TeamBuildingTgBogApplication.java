c9c49befc20381a2bf06df39e85b946d700ca69a
lexacv01@gmail.com <Agent3228>
2022-10-29 18:25:57 +0300

    Initial commit

---
+++ src/main/java/ru/growstreet/teambuildingtgbog/TeamBuildingTgBogApplication.java
@@ -0,0 +1,13 @@
+package ru.growstreet.teambuildingtgbog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamBuildingTgBogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamBuildingTgBogApplication.class, args);
+    }
+
+}
